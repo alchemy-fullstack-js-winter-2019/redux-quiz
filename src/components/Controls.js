90cc676854d93278f11e1fa264926e4d35528bc2
Kate Dameron <katerj33@gmail.com>
2019-03-04 15:54:21 -0800

    init npm and control component setup

---
+++ src/components/Controls.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Controls({ title, subtitle }) {
+  return (
+    <>
+    <input name={title} value={title}></input>
+    <input name={subtitle} value={subtitle}></input>
+    </>
+  );
+}
+
+Controls.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired
+};
