ad433259f3de50ad265b6ba49cec677fdd84b468
Marcy Silverman <marcysilve225@gmail.com>
2019-03-04 16:57:31 -0800

    updated tests

--- src/components/reducers/titleReducer.test.js
+++ src/components/reducers/titleReducer.test.js
@@ -1,15 +1,22 @@
 import reducer from './titleReducer';
+import { updateSubtitle } from '../actions/titleAction';
 
 describe('reducer test', () => {
   it('can update a title', () => {
     const state = {
-      title: ''
+      title: '',
+      subtitle: ''
     };
 
     const updatedState = reducer(state, {
       type: 'UPDATE_TITLE',
       payload: 'title1'
     });
+    const updatedSubtitle = reducer(state, {
+      type: 'UPDATE_SUBTITLE',
+      payload: 'title1'
+    });
     expect(updatedState).toEqual({ title: 'title1' });
+    expect(updatedSubtitle).toEqual({ subtitle: 'title1' });
   });
 });
--- src/components/selectors/titleSelector.test.js
+++ src/components/selectors/titleSelector.test.js
@@ -1,13 +1,18 @@
-import { getTitle } from './titleSelector';
+import { getTitle, getSubTitle } from './titleSelector';
 
 describe('selector test', () => {
   it('can test a title selector', () => {
     const state = {
       title: {
         title: 'title3'
+      },
+      subtitle: {
+        subtitle: 'title3'
       } 
     };
-    const selectedMarkdown = getTitle(state);
-    expect(selectedMarkdown).toEqual('title3');
+    const selectedTitle = getTitle(state);
+    const selectedSubtitle = getSubTitle(state);
+    expect(selectedTitle).toEqual({ title: 'title3' });
+    expect(selectedSubtitle).toEqual({ subtitle: 'title3' });
   });
 });
