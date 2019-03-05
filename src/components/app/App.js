1c340629d46ee6c9245d3c58f7939eba9b3fbbe3
Ivan <i.poblete915@gmail.com>
2019-03-04 17:00:02 -0800

    Display component working and displaying title and subtitle

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TitleSubtitleEditor from '../../containers/TitleSubtitleEditor';
+import TitleSubtitleDisplay from '../../containers/TitleSubtitleDisplay';
 
 function App() {
   return (
     <>
       <TitleSubtitleEditor />
+      <TitleSubtitleDisplay />
     </>
   );
 }
