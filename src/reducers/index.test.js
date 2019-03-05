44dd975c9fc1eb849750be531594c49023f58729
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-04 17:36:44 -0800

    Added reducer test

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,4 +1,19 @@
 import reducer from './index';
 import { updateTitle } from '../actions/header';
 
-describe('header reducer', )
+describe('header reducer', () => {
+  it('can update the title', () => {
+    const state = {
+      title: 'old title',
+      subtitle: 'old subtitle'
+    };
+
+    const updateTitleAction = updateTitle('new title', 'old title');
+    const updatedState = reducer(state, updateTitleAction);
+    expect(updatedState).toEqual({
+      title: 'new title',
+      subtitle: 'old title'
+    });
+    expect(updatedState.title).toHaveLength(1);
+  });
+});
