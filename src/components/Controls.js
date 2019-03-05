b6785d6e6a0951148d7b78ff0b381d9e3f69e1f4
Paige Gorry <paigeegorry@gmail.com>
2019-03-04 16:35:46 -0800

    basic styling

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './app/App.css';
 
 function Controls({ title, subtitle, onChange }) {
   return (
-    <>
-      <input type="text" name="title" value={title} onChange={onChange} />
-      <input type="text" name="subtitle" value={subtitle} onChange={onChange} />
-    </>
+    <div className={styles.controls}>
+      <label>
+      Title: 
+        <input type="text" name="title" value={title} onChange={onChange} />
+      </label>
+      <label>
+      Subtitle: 
+        <input type="text" name="subtitle" value={subtitle} onChange={onChange} />
+      </label>
+    </div>
   );
 }
 
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './app/App.css';
 
 function Display({
   title,
   subtitle
 }) {
   return (
-    <div>
+    <div className={styles.display}>
       <h1>{title}</h1>
       <h2>{subtitle}</h2>
     </div>
