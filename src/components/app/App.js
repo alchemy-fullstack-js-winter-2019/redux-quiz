d0a9c96c930859feb498b41d1ded0fe7d223c53e
Paige Gorry <paigeegorry@gmail.com>
2019-03-04 16:09:25 -0800

    able to see title and subtitle on display

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Controls from '../../containers/TitleSubtitleEditor';
+import Display from '../../containers/TitleSubtitleDisplay';
 
 export default function App() {
   return (
     <>
       <h1>Redux Quiz</h1>
       <Controls />
+      <Display />
     </>
   );
 }
