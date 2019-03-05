6728cd65b591a944c32c494e9c12550cbec64e23
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-04 16:26:32 -0800

    display rednering. edits with text boxes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TitleSubtitleEditor from '../../containers/TitleSubtitleEditor';
+import TitleSubtitleDisplay from '../../containers/TitleSubtitleDisplay';
 
 export default function App() {
   return (
     <>
       <TitleSubtitleEditor />
+      <TitleSubtitleDisplay />
     </>
   );
 }
